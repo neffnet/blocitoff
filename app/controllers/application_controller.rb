e0bd554a99c6a70767d861f542de1ecd0f22a902
neffnet <neff@neffnet.net>
2015-05-03 00:18:35 -0500

    add users_controller and users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << :name
       devise_parameter_sanitizer.for(:account_update) << :name
     end
+
+    def after_sign_in_path_for(resource)
+      user_path(resource)
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { confirmations: 'confirmations' }
+    resources :users, only: [:show]
+
+    
   root to: "welcome#index"
 end
