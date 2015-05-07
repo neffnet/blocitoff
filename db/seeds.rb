f196bf6ff9a8e31922a7c2fe0561f060c60ae3d4
neffnet <neff@neffnet.net>
2015-05-07 15:26:03 -0500

    fix for wrong users/items dependence direction, better seeds file, add todo rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@ require 'faker'
 #password: 	hello
 
 User.where(email: 'user@example.com').first_or_create do |u|
-	u.email = 'user@example.com'
-	u.name = 'Test User'
-	u.password = 'hello'
-	u.password_confirmation = 'hello'
+	update = {
+		name: 'Test User',
+		password: 'hello',
+		password_confirmation: 'hello'
+	}
+	u.update_attributes(update)
 	u.skip_confirmation!
 end
 
@@ -16,13 +18,16 @@ end
 u = User.find_by_email('user@example.com')
 u.items.each { |i| i.delete }
 
-# add five new items:
+# add some new items:
 5.times do
-	u = User.find_by_email('user@example.com') 
 	i = Item.create(user: u, name: Faker::Hacker.say_something_smart)
-	i.update_attributes!(created_at: Faker::Time.between(10.days.ago, Time.now))
+	i.update_attributes!(created_at: Faker::Time.backward(10))
+end
+5.times do
+	i = Item.create(user: u, name: Faker::Hacker.say_something_smart)
+	i.update_attributes!(created_at: Faker::Time.between(8.days.ago, 6.days.ago))
 end
 
 puts "created a Test User with id=#{u.id} and #{u.items.length} items."
 puts "email: #{u.email}"
-puts "password: hello" 
+puts "password: hello"
