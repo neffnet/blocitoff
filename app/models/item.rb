dffb88492ba03b5e4804f8bf83aa2c7c322bfcd9
neffnet <neff@neffnet.net>
2015-05-07 03:24:04 -0500

    show days remaining with color coded element

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,23 @@ class Item < ActiveRecord::Base
   
   validates :name, presence: true
   validates :user, presence: true
+
+  def days_left
+  	7 - age_in_days
+  end
+
+  def age_in_days
+  	( DateTime.now.to_date - created_at.to_date ).to_i
+  end
+
+  def color
+  	if days_left < 2
+  		"red"
+  	elsif days_left < 5
+  		"yellow"
+  	else
+  		"green"
+  	end
+  end
+
 end
